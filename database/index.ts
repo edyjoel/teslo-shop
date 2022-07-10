0fa12741d14c5c65afed3d0aeb908e1421b94ff7
Edy <edyxicon@gmail.com>
2022-07-10 13:28:20 -0600

    Database and RestFul

---
+++ database/index.ts
@@ -0,0 +1,4 @@
+export * as db from './db'
+export * from './constants'
+
+export * as seedDatabase from './products'
---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database';
+import { IProduct } from '../../../interfaces';
+import { Product } from '../../../models';
+
+type Data =
+| {message: string}
+| IProduct
+
+export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
+
+  switch (req.method) {
+    case 'GET':
+      return getProductBySlug(req, res)
+    default:
+      return res.status(400).json({message: 'Invalid method'});
+  }
+
+}
+
+const getProductBySlug = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const {slug} = req.query;
+  await db.connect()
+  const product = await Product.findOne({slug})
+
+  if(!product) {
+    await db.disconnect()
+    return res.status(404).json({message: 'Product not found'})
+  }
+  res.status(200).json(product)
+}
