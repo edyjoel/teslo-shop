474783fc2032c4277cfe64688cdef6c0c0bcdf5b
Edy <edyxicon@gmail.com>
2022-12-21 17:50:49 -0600

    test auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,93 +1,61 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import Credentials from "next-auth/providers/credentials";
-
-import { dbUsers } from "../../../database";
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import Credentials from 'next-auth/providers/credentials';
+import { dbUsers } from '../../../database';
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
-    // ...add more providers here
-
     Credentials({
-      name: "Custom Login",
+      name: 'credentials',
       credentials: {
-        email: {
-          label: "Correo:",
-          type: "email",
-          placeholder: "correo@google.com",
-        },
-        password: {
-          label: "Contraseña:",
-          type: "password",
-          placeholder: "Contraseña",
-        },
+        email: {label: 'Correo:', type: 'email', placeholder: 'correo@google.com'},
+        password: {label: 'Contraseña:', type: 'password', placeholder: 'Contraseña'},
       },
-      async authorize(credentials) {
-        console.log({ credentials });
-        // return { name: 'Juan', correo: 'juan@google.com', role: 'admin' };
-
-        return await dbUsers.checkUserEmailPassword(
-          credentials!.email,
-          credentials!.password
-        );
-      },
+      async authorize(credentials): Promise<any> {
+        // return {name: 'Juan', correo: 'juan@google.com', role: 'admin'}
+        return await dbUsers.checkUserEmailPassword(credentials!.email, credentials!.password)
+      }
     }),
-
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
     }),
   ],
-
-  // Custom Pages
   pages: {
-    signIn: "/auth/login",
-    newUser: "/auth/register",
+    signIn: '/auth/login',
+    newUser: '/auth/register',
   },
 
-  // Callbacks
   jwt: {
-    // secret: process.env.JWT_SECRET_SEED, // deprecated
+
   },
 
   session: {
-    maxAge: 2592000, /// 30d
-    strategy: "jwt",
-    updateAge: 86400, // cada día
+    maxAge: 2592000, // 1 month
+    strategy: 'jwt',
+    updateAge: 86400, // 1 day
   },
 
   callbacks: {
-    async jwt({ token, account, user }) {
-      // console.log({ token, account, user });
-
-      if (account) {
+    async jwt({token, account, user}) {
+      if(account) {
         token.accessToken = account.access_token;
 
         switch (account.type) {
-          case "oauth":
-            token.user = await dbUsers.oAUthToDbUser(
-              user?.email || "",
-              user?.name || ""
-            );
+          case 'oauth':
+            token.user = await dbUsers.oAuthToDbUser(user?.email || '', user?.name || '');
             break;
-
-          case "credentials":
-            token.user = user;
+          case 'credentials':
+            token.user = user
             break;
         }
       }
-
-      return token;
+      return token
     },
-
-    async session({ session, token, user }) {
-      // console.log({ session, token, user });
-
-      session.accessToken = token.accessToken;
+    async session({session, token, user}) {
+      (session as any).accessToken = token.accessToken;
       session.user = token.user as any;
-
-      return session;
-    },
-  },
-});
+      return session
+    }
+  }
+})
